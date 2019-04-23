f0d859b50878fef982cdb5f9c1333a7027a5ab52
Giovani <giovani.hbertuzzo@gmail.com>
2019-04-23 20:57:50 -0300

    Produtor consumidor

---
+++ src/slide6/ProdutorConsumidor/Objeto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slide6.ProdutorConsumidor;
+
+/**
+ *
+ * @author a1602020
+ */
+public class Objeto {
+    
+    public String buffer;    
+
+    public Objeto(String buffer) {
+        this.buffer = buffer;
+    }
+
+    public String getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(String buffer) {
+        this.buffer = buffer;
+    }    
+    
+}
---
+++ src/slide6/ProdutorConsumidor/ProducerConsumer.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slide6.ProdutorConsumidor;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author a1602020
+ */
+public class ProducerConsumer {
+    
+    public static void main(String[] args) {
+        
+        Objeto objeto = new Objeto("");
+        
+        Thread producer = new Thread(() -> {
+            while (true) {
+                Random rand = new Random();                
+                int n = rand.nextInt(5);
+                int tempo = n * 1000;
+                try {
+                    Thread.currentThread().sleep(tempo);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                synchronized (objeto) {
+                    if (objeto.getBuffer().isEmpty()) {
+                        objeto.setBuffer("Novo Objeto");
+                        System.out.println("Produtor say: New Object produced -> " + objeto.getBuffer());
+
+                        objeto.notify();
+
+                    } else {
+                        try {
+                            objeto.wait();
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+                        }                        
+                    }
+                }
+            }
+        });
+        producer.start();
+        
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                synchronized (objeto) {
+                    if (!objeto.getBuffer().isEmpty()) {
+                        System.out.println("Consumer say: New Object consumed " + objeto.getBuffer());
+                        objeto.setBuffer("");
+                        objeto.notify();
+                    } else {
+                        try {
+                            objeto.wait();
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+        });
+        consumer.start();
+    }
+    
+}
