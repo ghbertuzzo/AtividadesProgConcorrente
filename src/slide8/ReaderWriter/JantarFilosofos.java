8f898738dbf42d53f6390f7784117655a03c144b
unknown <giovani.hbertuzzo@gmail.com>
2019-05-17 21:16:12 -0300

    Janta filo solucao 1

--- src/slide8/ReaderWriter/JantarFilosofos.java
+++ src/slide8/ReaderWriter/JantarFilosofos.java
@@ -19,10 +19,6 @@ public class JantarFilosofos {
 
     public static void main(String[] args) {
         JantarFilosofos jf = new JantarFilosofos(5);
-        for (int i = 0; i < jf.listFil.length; i++) {
-            jf.listFil[i].run();
-        }
-
     }
 
     public JantarFilosofos(int initResources) {
@@ -35,25 +31,45 @@ public class JantarFilosofos {
         for (int i = 0; i < initResources; i++) {
             forkLibert[i] = true;
         }
+        listFil = new Filosofo[initResources];
         for (int i = 0; i < initResources; i++) {
             listFil[i] = new Filosofo(i, this);
         }
+        stort(listFil);
+    }
+    public void eating(int idFilosofo) {
+        System.out.println("Filosofo " + idFilosofo + " comendo.");
     }
 
-    public synchronized void take(int idFilosofo) throws InterruptedException {
+    public void thinking(int idFilosofo) throws InterruptedException{
+        System.out.println("Filosofo " + idFilosofo + " pensando.");
+        Integer rand = new Random().nextInt(100);
+        sleep(rand);
+    }
+
+    public void take(int idFilosofo) throws InterruptedException {
         if (forkLibert[idFilosofo] && forkLibert[(idFilosofo + 1) % this.numberResources]) {
             fork[idFilosofo].acquire();
+            System.out.println("Filosofo "+idFilosofo+" take first fork");
             fork[(idFilosofo + 1) % this.numberResources].acquire();
+            System.out.println("Filosofo "+idFilosofo+" take second fork");
             forkLibert[idFilosofo] = false;
             forkLibert[(idFilosofo + 1) % this.numberResources] = false;
         }
     }
 
-    public synchronized void release(int idFilosofo) {
+    public void release(int idFilosofo) {
         forkLibert[idFilosofo] = true;
         forkLibert[(idFilosofo + 1) % this.numberResources] = true;
         fork[idFilosofo].release();
         fork[(idFilosofo + 1) % this.numberResources].release();
+    }
+
+    private void stort(Filosofo[] listFil) {
+        for(int i=0;i<listFil.length;i++){
+            listFil[i].run();
+            System.out.println("entro ak kkk");
+        }
     }
 
     public class Filosofo implements Runnable {
@@ -71,7 +87,7 @@ public class JantarFilosofos {
             int cont = 0;
             while (cont < 10) {
                 try {
-                    thinking(this.id);
+                    jf.thinking(this.id);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(JantarFilosofos.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -80,21 +96,13 @@ public class JantarFilosofos {
                 } catch (InterruptedException ex) {
                     Logger.getLogger(JantarFilosofos.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                eating(this.id);
+                jf.eating(this.id);
                 cont++;
                 jf.release(this.id);
             }
         }
 
-        public void eating(int idFilosofo) {
-            System.out.println("Filosofo " + idFilosofo + " comendo.");
-        }
-
-        public void thinking(int idFilosofo) throws InterruptedException {
-            System.out.println("Filosofo " + idFilosofo + " pensando.");
-            Integer rand = new Random().nextInt(100);
-            sleep(rand);
-        }
+        
     }
 
 }
